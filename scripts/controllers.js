1d56783c2f6567125228c5c8268b9ba96c720f98
Valentin Kirilov <val.kirilov@gmail.com>
2015-08-23 11:18:37 +0300

    Moving the favicons to the root and uodaing the cache

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -12,7 +12,7 @@ angular.module('whatToDo.controllers', [])
     var currentDate = new Date();
     $rootScope.app = {
         year: currentDate.getFullYear(),
-        version: '2.0.1'
+        version: '2.0.2'
     };
 
     $rootScope.firebaseRef;
